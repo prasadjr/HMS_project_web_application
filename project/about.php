1b969db12fc716e07903cf622c3704c1d7089937
prasadjr <126258335+prasadjr@users.noreply.github.com>
2024-10-04 21:33:30 +0530

    Add files via upload
    This is  application still under process

---
+++ project/about.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    
+    <!-- custom css file -->
+    <link rel="stylesheet" href="css/style.css"/>
+
+    <!-- icon links  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+
+</head>
+<body>
+
+<!-- header section starts -->
+<section class="header">
+    <a href="home.php" class="logo">Bee-Trip</a>
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+    <div id="menu-btn" class="fas fa-bars"></div>
+</section>
+<!-- header section ends -->
+<!-- 26:44  -->
+<div class="heading" style="background: url(images/heading-bg-1.jpeg) no-repeat center center; background-size: cover;">
+    <h1>about us</h1>
+</div>
+
+
+<!-- footer section starts -->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="tel:+917892849676"><i class="fas fa-phone"></i> +91 7892849676</a>
+            <a href="#"><i class="fas fa-phone"></i>+91 9844012104</a>
+            <a href="#"><i class="fas fa-envelope"></i>prasadjrpc@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i>Mysuru,India-570018</a>
+        </div>
+
+         <div class="box"> <!-- Corrected from "box-class" to "box" -->
+            <h3>follow-on</h3>
+            <a href="#"><i class="fas fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fas fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fas fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fas fa-Linkedin"></i>linkedIn</a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>Mr. <a href = "https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://in.linkedin.com/in/prasad-j-r-2b3571259&ved=2ahUKEwjR3pmousKIAxWp2TgGHb3KJcEQFnoECCIQAQ&usg=AOvVaw3NE6QzYLsvHCPf6WdMZA-k">Prasad J R</a>, Undergradute student @ NIE,Mysuru </span> | all rights reserved</div>
+    
+</section>
+<!-- footer section ends -->
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
